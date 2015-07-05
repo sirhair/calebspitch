284b8becfa1ad813ec9d44eb62f810d35bb66605
Edward Alonzo <edward@thenewe.com>
2015-07-05 11:40:14 -0500

    remove genesis add newe base

--- functions.php
+++ functions.php
@@ -1,31 +1,37 @@
 <?php
-//* Start the engine
-include_once( get_template_directory() . '/lib/init.php' );
-
-//* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Calebs Pitch Wordpress/Genesis theme' );
-
-define( 'CHILD_THEME_URL', 'https://github.com/sirhair/calebspitch' );
-define( 'CHILD_THEME_VERSION', '0.1' );
-
-//* Enqueue Google Fonts
-add_action( 'wp_enqueue_scripts', 'add_fonts' );
-function add_fonts() {
-
-	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
-
+/*-----------------------enqueue CSS fields-----------------------*/
+function foundation_css(){
+	wp_enqueue_style(
+		'foundation',
+		get_template_directory_uri() . '/css/foundation.css'
+		);
 }
+add_action('wp_enqueue_scripts', 'foundation_css');
 
-//* Add HTML5 markup structure
-add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+function normalize_css(){
+	wp_enqueue_style(
+		'normalize',
+		get_template_directory_uri() . '/css/normalize.css'
+		);
+}
+add_action('wp_enqueue_scripts', 'normalize_css');
 
-//* Add viewport meta tag for mobile browsers
-add_theme_support( 'genesis-responsive-viewport' );
 
-//* Add support for custom background
-add_theme_support( 'custom-background' );
+/*-----------------------enable widgets-----------------------*/
 
-//* Add support for 3-column footer widgets
-add_theme_support( 'genesis-footer-widgets', 3 );
+function blank_widgets_init(){
+	register_sidebar(array(
+		'name' => ('Main Sidebar'),
+		'id' =>'main-sidebar',
+		'description' => 'Widget for our sidebar on pages',
+		'before_widget' => '<div class="widget-sidbar">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>'
+		)
+	);
+}
+add_action('widgets_init','blank_widgets_init');
 
-//* Add theme minified scripts
+
+?>

aa0d3b353a6d6b4d84bc41905ccdf035389ea737
Kelly Ngo <kellyngo726@gmail.com>
2015-07-10 13:49:10 -0400

    add content and styles to get involved section

--- index.php
+++ index.php
@@ -5,12 +5,37 @@
 					<div class="storyText">
 						<h2>The Caleb's Pitch Story</h2>
 						<p>Caleb's Pitch is a nonprofit organization founded to honor the inspirational spirit of Caleb Jacobbe. We are dedicated to creating positive experiences and enhancing quality of life for children and their families while being treated for serious conditions at the hospital.</p>
-							<button>Learn More</button>
+							<a href="#"><button class="blueButton">Learn More</button></a>
 					</div>
 					<div class="video">
 						<iframe width="560" height="315" src="https://www.youtube.com/embed/HoG1NQoU04M" frameborder="0" allowfullscreen></iframe>
 					</div>
 				</section>
+
+			<section class="getInvolved">
+				<h2>Get Involved</h2>
+					<div class="getInvolvedBadge">
+						<div class="circleGreen"></div>
+						<h3>Donate</h3>
+						<p>Some text about donating money and how it's awesome.</p>
+					</div>
+					<div class="getInvolvedBadge">
+						<div class="circleBlue"></div>
+						<h3>Volunteer</h3>
+						<p>Some text about volunteering and how it's awesome.</p>
+					</div>
+					<div class="getInvolvedBadge">
+						<div class="circleSalmon"></div>
+						<h3>Sponsor</h3>
+						<p>Some text about sponsoring Caleb's Pitch and how it's awesome.</p>
+					</div>
+			</section>
+
+
+
+
+			</section>
+
 		</main>
 
 <?php get_footer(); ?>

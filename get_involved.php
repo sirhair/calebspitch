b15172d330223b8149e373589a73a90b5a7bc6af
Kelly Ngo <kellyngo726@gmail.com>
2015-07-11 11:30:21 -0400

    get involved

---
+++ get_involved.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Template Name: Get Involved Landing Page
+*/
+?>
+
+<?php get_header(); ?>
+<main class="main" >
+    <section class="layout getInvolvedPage">
+
+        <h1>How You Can Get Involved</h1>
+
+        <section class="giVolunteer fullwidth">
+            <div class="giVolunteerText">
+                <h2>Volunteer</h2>
+                    <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
+            </div>
+            <div class="giVolunteerVideo">
+                <iframe src="https://www.youtube.com/embed/IiLjwwUHq0I" frameborder="0" allowfullscreen></iframe>
+            </div>
+
+        </section>
+
+        <section class="giDonate fullwidth">
+            <div class="giDonateText">
+                <h2>Donate</h2>
+                    <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
+            </div>
+            <div class="giDonateImage">
+            </div>
+        </section>
+
+        <section class="giSponsor fullwidth">
+            <div class="giSponsorText">
+                <h2>Sponsor</h2>
+                    <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
+            </div>
+            <div class="giSponsorImage">
+            </div>
+        </section>
+
+    </section>
+</section>
+
+<?php get_footer(); ?>

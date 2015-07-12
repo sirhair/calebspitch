fd28aa263e2b908516e6389b0f0071c7589d0a4d
Kelly Ngo <kellyngo726@gmail.com>
2015-07-11 21:22:04 -0400

    add sponsor form

--- get_involved.php
+++ get_involved.php
@@ -7,38 +7,51 @@ Template Name: Get Involved Landing Page
 <?php get_header(); ?>
 <main class="main" >
     <section class="layout getInvolvedPage">
-
         <h1>How You Can Get Involved</h1>
+    </section>
 
         <section class="giVolunteer fullwidth">
-            <div class="giVolunteerText">
-                <h2>Volunteer</h2>
-                    <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
+            <div class="layout">
+                <div class="giVolunteerText">
+                    <h2>Volunteer</h2>
+                        <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
+                        <a href="#"><button class="greenButton">Sign Me Up!</button></a>
+                </div>
+                <div class="giVolunteerVideo">
+                    <iframe src="https://www.youtube.com/embed/IiLjwwUHq0I" frameborder="0" allowfullscreen></iframe>
+                </div>
             </div>
-            <div class="giVolunteerVideo">
-                <iframe src="https://www.youtube.com/embed/IiLjwwUHq0I" frameborder="0" allowfullscreen></iframe>
-            </div>
-
         </section>
+
+        <section class="layout"></section>
 
         <section class="giDonate fullwidth">
-            <div class="giDonateText">
-                <h2>Donate</h2>
-                    <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
-            </div>
-            <div class="giDonateImage">
+            <div class="layout">
+                <div class="giDonateText">
+                    <h2>Donate</h2>
+                        <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
+                        <a href="#"><button class="blueButton">Learn More</button></a>
+
+                </div>
+                <div class="giDonateImage">
+                </div>
             </div>
         </section>
+
+        <section class="layout"></section>
 
         <section class="giSponsor fullwidth">
-            <div class="giSponsorText">
-                <h2>Sponsor</h2>
-                    <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
-            </div>
-            <div class="giSponsorImage">
+            <div class="layout">
+                <div class="giSponsorText">
+                    <h2>Sponsor</h2>
+                        <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
+                        <a href="#"><button class="blueButton">Learn More</button></a>
+                </div>
+                <div class="giSponsorImage">
+                </div>
             </div>
         </section>
-
+    <section class="layout">
     </section>
 </main>
 

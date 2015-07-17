feada25024aab49ff1af75baf56191eb77cbd031
Edward Alonzo <edward@thenewe.com>
2015-07-16 20:59:05 -0500

    playing with footer

--- footer.php
+++ footer.php
@@ -3,6 +3,20 @@
 
 			<section class="layout">
 
+  
+        <div class="fourths footsocial"> <!-- social div -->
+          <h2>Connect</h2>
+        </div>   <!-- social div -->
+  
+        <div class="fourths footnav"> <!-- nav div -->
+          <?php wp_nav_menu(array('theme_location' => 'footer', 'container _class' =>'nav_menu'));?>
+
+        </div> <!-- nav div -->
+
+        <div class="fourths footcopy"><!--  copyright div -->
+          <p>&copy; <?php echo date("Y") ?> Caleb's Pitch</p> 
+        </div><!--  copyright div -->
+
         <div class="fourths footlogo"><!-- logo div -->
         
             <a href="<?php $url = home_url( '/' ); echo $url; ?>">
@@ -11,18 +25,6 @@
         
         </div> <!-- logo div -->
 
-        <div class="fourths footnav"> <!-- nav div -->
-          <?php wp_nav_menu(array('theme_location' => 'footer', 'container _class' =>'nav_menu'));?>
-
-        </div> <!-- nav div -->
-  
-        <div class="fourths footsocial"> <!-- social div -->
-          <h2>Connect</h2>
-        </div>   <!-- social div -->
-  
-        <div class="fourths footcopy"><!--  copyright div -->
-          <p>&copy; <?php echo date("Y") ?> Caleb's Pitch</p> 
-        </div><!--  copyright div -->
 
 			</section>
 

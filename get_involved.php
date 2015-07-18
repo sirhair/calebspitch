363bd0fb563425e26cd78719703704043d5e985d
Veronica Vilar <veronicavilar246@gmail.com>
2015-07-17 22:00:04 -0400

    added tribe-events-promo class hidden line #344

--- get_involved.php
+++ get_involved.php
@@ -1,58 +1,38 @@
-<?php
-/*
-Template Name: Get Involved Landing Page
-*/
-?>
-
 <?php get_header(); ?>
-<main class="main" >
-    <section class="layout getInvolvedPage">
-        <h1>How You Can Get Involved</h1>
-    </section>
-
-        <section class="giVolunteer fullwidth">
-            <div class="layout">
-                <div class="giVolunteerText">
-                    <h2>Volunteer</h2>
-                        <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
-                        <a href="#"><button class="greenButton">Sign Me Up!</button></a>
-                </div>
-                <div class="giVolunteerVideo">
-                    <iframe src="https://www.youtube.com/embed/IiLjwwUHq0I" frameborder="0" allowfullscreen></iframe>
-                </div>
-            </div>
-        </section>
-
-        <section class="layout"></section>
-
-        <section class="giDonate fullwidth">
-            <div class="layout">
-                <div class="giDonateText">
-                    <h2>Donate</h2>
-                        <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
-                        <a href="#"><button class="blueButton">Learn More</button></a>
-
-                </div>
-                <div class="giDonateImage">
-                </div>
-            </div>
-        </section>
-
-        <section class="layout"></section>
-
-        <section class="giSponsor fullwidth">
-            <div class="layout">
-                <div class="giSponsorText">
-                    <h2>Sponsor</h2>
-                        <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
-                        <a href="#"><button class="blueButton">Learn More</button></a>
-                </div>
-                <div class="giSponsorImage">
-                </div>
-            </div>
-        </section>
-    <section class="layout">
-    </section>
-</main>
-
-<?php get_footer(); ?>
+ 
+ <main class="main">
+-    <section class="events">
++    <section class="layout events">
+ 
+            <?php if (have_posts()) :
+            while (have_posts()) : the_post(); ?> <!--[data context]-->
+                <?php the_content();
+              endwhile;
+            endif; ?>
++   </section>
+ 
+-             
++           <div class="getInvolved">
++               <section class="layout">
++                   <h2>Get Involved</h2>
++                       <div class="getInvolvedBadge">
++                           <img class="homeSplat hvr-float-shadow" alt="Caleb's Pitch donate icon" src="<?php echo get_stylesheet_directory_uri(); ?>/images/green_splat.png">
++                           <h3>Donate</h3>
++                           
++                       </div>
++                       <div class="getInvolvedBadge">
++                           <img class="homeSplat hvr-float-shadow" alt="Caleb's Pitch volunteer icon" src="<?php echo get_stylesheet_directory_uri(); ?>/images/blue_splat.png">
++                           <h3>Volunteer</h3>
++                           
++                       </div>
++                       <div class="getInvolvedBadge">
++                           <img class="homeSplat hvr-float-shadow" alt="Caleb's Pitch sponsor icon" src="<?php echo get_stylesheet_directory_uri(); ?>/images/pink_splat.png">
++                           <h3>Sponsor</h3>
++                           
++                       </div>
++               </section>
++           </div>
++               
+     </section>
+ </main>
+         

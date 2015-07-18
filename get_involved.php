36572b3ccdc8c69bfed701f805f234202f7f7cfa
Kelly Ngo <kellyngo726@gmail.com>
2015-07-18 14:47:27 -0400

    get involved edits

--- get_involved.php
+++ get_involved.php
@@ -15,7 +15,7 @@ Template Name: Get Involved Landing Page
                 <div class="giVolunteerText">
                     <h2>Volunteer</h2>
                         <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
-                        <a href="#"><button class="greenButton">Sign Me Up!</button></a>
+                        <a href="#"><button class="blueButton">Sign Me Up!</button></a>
                 </div>
                 <div class="giVolunteerVideo">
                     <iframe src="https://www.youtube.com/embed/IiLjwwUHq0I" frameborder="0" allowfullscreen></iframe>
@@ -29,8 +29,8 @@ Template Name: Get Involved Landing Page
             <div class="layout">
                 <div class="giDonateText">
                     <h2>Donate</h2>
-                        <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
-                        <a href="#"><button class="blueButton">Learn More</button></a>
+                        <p>Without the generous support of our donors Caleb's Pitch wouldn't be able to reach as many families or host as many events. You contribution to Caleb's Pitch helps to buy paint for syringe paintings, canvases, and other supplies we need to keep putting smiles on kids' faces.</p>
+                        <a href="#"><button class="greenButton">Donate</button></a>
 
                 </div>
                 <div class="giDonateImage">
@@ -44,8 +44,8 @@ Template Name: Get Involved Landing Page
             <div class="layout">
                 <div class="giSponsorText">
                     <h2>Sponsor</h2>
-                        <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraiser. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
-                        <a href="#"><button class="blueButton">Learn More</button></a>
+                        <p>By sponsoring an event with Caleb's Pitch you help us reach out the community and build our capacity to reach more children and families. Reliable contributions allows us to plan and expand! Please consider sponsoring an event with us today.</p>
+                        <a href="#"><button class="pinkButton">Sponsor Us</button></a>
                 </div>
                 <div class="giSponsorImage">
                 </div>

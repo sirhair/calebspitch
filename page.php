f72e2f9fd7f514f2a0996b54ed438780ec9924e6
Edward Alonzo <edward@thenewe.com>
2015-07-18 20:16:57 -0500

    change h2s in page and post to h1, style h1s

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
 		<section class="layout">
 			<?php if (have_posts()):
 				while(have_posts()) : the_post(); ?>
-					<h2><?php the_title(); ?> </h2>
+					<h1><?php the_title(); ?> </h1>
 					<?php the_content();
 				endwhile; 
 				endif; ?>

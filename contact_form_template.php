8d51586a7cb8f8ad3653f8cf3242f8f6b275ca1f
Kelly Ngo <kellyngo726@gmail.com>
2015-07-19 10:19:40 -0400

    add thank you and contact

---
+++ contact_form_template.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Template Name: Contact Form Template
+*/
+?>
+
+<?php get_header(); ?>
+<main class="main" >
+    <section class="layout">
+
+<!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup" class="contactForm">
+    <form action="//kellyngo.us11.list-manage.com/subscribe/post?u=e17dc775841d90c94453ee785&amp;id=8ca46ed13d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+        <h2>We Want To Hear From You!</h2>
+    <!--<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>-->
+    <div class="mc-field-group">
+        <label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label>
+        <br />
+        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+    </div>
+    <div class="mc-field-group">
+        <label for="mce-FNAME">First Name </label>
+        <br />
+        <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+    </div>
+    <div class="mc-field-group">
+        <label for="mce-LNAME">Last Name </label>
+        <br />
+        <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+    </div>
+    <div class="mc-field-group">
+        <label for="mce-MESSAGE">Your Message</label>
+        <br />
+        <textarea rows="4" cols="50" type="text" name="MESSAGE" class="" id="mce-MESSAGE"></textarea>
+    </div>
+        <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style="display:none"></div>
+            <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <br />
+        <div style="position: absolute; left: -5000px;"><input type="text" name="b_e17dc775841d90c94453ee785_8ca46ed13d" tabindex="-1" value=""></div>
+        <div class="clear"><input type="submit" value="Send My Message" name="subscribe" id="mc-embedded-subscribe" class="formButton contactButton"></div>
+        </div>
+    </form>
+</div>
+
+<!--End mc_embed_signup-->
+
+    </section>
+</main>
+
+<?php get_footer(); ?>
--- get_involved.php
+++ get_involved.php
@@ -9,7 +9,7 @@
                 <div class="giText">
                     <h2>Volunteer</h2>
                         <p>You can volunteer with Caleb's Pitch at our syringe art events or at any of our fundraisers. We're always in need of more volunteers so that we can expand our progam to as many kids and families as possible. Sign up today and we'll be  in touch with how you can help.</p>
-                        <a href="#"><button class="blueButton">Sign Me Up!</button></a>
+                        <a href="http://www.cp.thenewe.com/volunteer/"><button class="blueButton">Sign Me Up!</button></a>
                 </div>
                 <div class="giImage">
                     <iframe src="https://www.youtube.com/embed/IiLjwwUHq0I" frameborder="0" allowfullscreen></iframe>
@@ -24,7 +24,7 @@
                 <div class="giText">
                     <h2>Donate</h2>
                         <p>Without the generous support of our donors Caleb's Pitch wouldn't be able to reach as many families or host as many events. You contribution to Caleb's Pitch helps to buy paint for syringe paintings, canvases, and other supplies we need to keep putting smiles on kids' faces.</p>
-                        <a href="#"><button class="greenButton">Donate</button></a>
+                        <a href="http://www.cp.thenewe.com/donate/"><button class="greenButton">Donate</button></a>
 
                 </div>
                 <div class="giImage">
@@ -40,7 +40,7 @@
                 <div class="giText">
                     <h2>Sponsor</h2>
                         <p>By sponsoring an event with Caleb's Pitch you help us reach out the community and build our capacity to reach more children and families. Reliable contributions allows us to plan and expand! Please consider sponsoring an event with us today.</p>
-                        <a href="#"><button class="pinkButton">Sponsor Us</button></a>
+                        <a href="http://www.cp.thenewe.com/sponsor-sign-up/"><button class="pinkButton">Sponsor Us</button></a>
                 </div>
                 <div class="giImage">
                     <img alt="Caleb's Pitch event raffle tickets" src="<?php echo get_stylesheet_directory_uri(); ?>/images/sponsor-image.jpg">

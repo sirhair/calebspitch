39c800e8676686652832f3e648920805e35d2e5d
Edward Alonzo <edward@thenewe.com>
2015-07-19 14:53:29 -0500

    add social icons to footer

--- footer.php
+++ footer.php
@@ -6,9 +6,29 @@
   
         <div class="fourths footsocial"> <!-- social div -->
           <h2>Connect</h2>
+            <a href="https://www.facebook.com/CalebsPitch" title="Caleb's Pitch Facebook">
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x cpBlue"></i>
+                <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+              </span>
+            </a>  
+            <a href="https://twitter.com/CalebsPitch" title="Caleb's Pitch Twitter">
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x cpSalmon"></i>
+                <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+              </span>
+            </a>  
+<!--             <a href="https://www.instagram.com/CalebsPitch" title="Caleb's Pitch Instagram">
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x cpYellow"></i>
+                <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+              </span>
+            </a>   -->
         </div>   <!-- social div -->
   
         <div class="fourths footnav"> <!-- nav div -->
+          <h2>Menu</h2>
+
           <?php wp_nav_menu(array('theme_location' => 'footer', 'container _class' =>'nav_menu'));?>
 
         </div> <!-- nav div -->

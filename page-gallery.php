3cc5ca6f7d8f2e6f7c0f8a2fced940740748ebab
Edward Alonzo <edward@thenewe.com>
2015-07-20 18:21:23 -0500

    h1 for galler

--- page-gallery.php
+++ page-gallery.php
@@ -11,7 +11,7 @@ Template name: Gallery Template
 
         	<?php if (have_posts()) :
 			while (have_posts()) : the_post(); ?> <!--[data context]-->
-            	<h2><?php the_title(); ?></h2>
+            	<h1><?php the_title(); ?></h1>
             	<?php the_content();
 			  endwhile;
 			endif; ?>

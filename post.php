93052abe5a5c6e04746d2cec25783c75e52f696b
Edward Alonzo <edward@thenewe.com>
2015-07-24 15:21:15 -0500

    add blog page nav, fix layout to rems

--- post.php
+++ post.php
@@ -5,8 +5,12 @@
 			<?php if (have_posts()):
 				while(have_posts()) : the_post(); ?>
 					<h1><?php the_title(); ?> </h1>
-					<?php the_content();
-				endwhile; 
+					<?php the_content();?>
+					<div class="navigation">
+						<p><?php posts_nav_link(); ?></p>
+					</div>
+
+				<?php endwhile; 
 				endif; ?>
 
 		</section>

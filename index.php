97d48817165da2ead35a2916c92e1445ffa45691
Edward Alonzo <edward@thenewe.com>
2015-07-24 16:55:05 -0500

    change blog to excerpt

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 			<?php if (have_posts()):
 				while(have_posts()) : the_post(); ?>
 					<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a> </h2>
-					<?php the_content();
+					<?php the_excerpt();
 				endwhile; 
 				endif; ?>
 

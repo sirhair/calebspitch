cbeaa7e7644f998b95769f39a48361bce96c11a8
Kelly Ngo <kellyngo726@gmail.com>
2015-07-24 11:27:34 -0400

    fix forms

--- sponsor_form_template.php
+++ sponsor_form_template.php
@@ -9,14 +9,14 @@ Template Name: Sponsor Form Template
     <section class="layout">
 
     <section class="thirdwidth formText">
-        <img class="formSplat hvr-float-shadow" alt="Caleb's Pitch volunteer icon" src="<?php echo get_stylesheet_directory_uri(); ?>/images/pink_splat.png">
+        <img class="formSplat" alt="Caleb's Pitch volunteer icon" src="<?php echo get_stylesheet_directory_uri(); ?>/images/pink_splat.png">
         <h2>Sponsor Caleb's Pitch Today!</h2>
-            <p>By sponsoring an event with Caleb's Pitch you help us reach out the community and build our capacity to reach more children and families. Use the form to the right to sign up, and we'll contact you to talk about how your business can make a lasting contribution to Caleb's Pitch.</p>
+            <p>By sponsoring an event with Caleb's Pitch you help us reach out the community and build our capacity to help more children and families. Your business can sponsor Caleb's Pitch by contibuting funds to help us put on our events, or by making in-kind donations. Use the form to the right to sign up, and we'll contact you to talk about how your business can make a lasting contribution to Caleb's Pitch.</p>
     </section>
 
 <!-- Begin MailChimp Signup Form -->
 <div id="mc_embed_signup" class="sponsorForm">
-    <form action="//kellyngo.us11.list-manage.com/subscribe/post?u=e17dc775841d90c94453ee785&amp;id=8ca46ed13d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <form action="//thenewe.us11.list-manage.com/subscribe/post?u=e17dc775841d90c94453ee785&amp;id=8ca46ed13d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll">
         <h2>Sponsor Sign Up</h2>
     <!--<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>-->

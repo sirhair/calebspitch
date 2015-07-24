038444278cff3b78c663a2b8044582329fca0d85
Edward Alonzo <edward@thenewe.com>
2015-07-24 17:04:34 -0500

    update readme to (read me)

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ add_action( 'init', 'register_my_menus' );
 
 //readmore links
 function new_excerpt_more( $more ) {
-    return ' <a class="read-more" href="' . get_permalink( get_the_ID() ) . '">' . __( 'Read More', 'your-text-domain' ) . '</a>';
+    return ' <a class="read-more" href="' . get_permalink( get_the_ID() ) . '">' . __( '(Read More)', 'your-text-domain' ) . '</a>';
 }
 add_filter( 'excerpt_more', 'new_excerpt_more' );
 

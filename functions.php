fbd4cf35e3460201b2c93564b3adab806437a604
Kelly Ngo <kellyngo726@gmail.com>
2015-07-25 14:30:50 -0400

    merge

--- functions.php
+++ functions.php
@@ -11,6 +11,11 @@ function register_my_menus() {
 add_action( 'init', 'register_my_menus' );
 
 
+//readmore links
+function new_excerpt_more( $more ) {
+    return ' <a class="read-more" href="' . get_permalink( get_the_ID() ) . '">' . __( '(Read More)', 'your-text-domain' ) . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
 
 /*-----------------------enable widgets-----------------------*/
 
